50ddbfdcdf7d4fee4aeb6617afee48ddb1a14e7f
kewynlong <marcelojapao21@gmail.com>
2025-11-26 22:43:48 -0300

    Add files via upload

--- atutil.c
+++ atutil.c
@@ -1,42 +1,86 @@
 // auto install linux mint
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include "kew.h"
 
 int main(void){
-	system("clear");
+	//localidade do teclado
 	setlocale(LC_ALL, "");
 
 	//variaveis
 	int desi=0;
+	int d[10];
+	char resf = 's';
 
-	printf("seja bem vindo ao atutil minha feramenta de auto istalação para linux(Somente para ubuntu e linux mint, mas vou espandir para outras plataformas)\n");
-	
-	printf("(1)Produtividade\n(2)jogos(não está pronto ainda)\n(3)dev(não está pronto ainda)\n(4)sair(não esta pronto ainda)\n");
-	scanf("%d", &desi);
-	if(desi == 1){
+	do{
+
+		 //começo do promgrama
+
 		system("clear");
-		printf("qual programas você deseja instalar?\n (1) ONLYOFFICE\n (2) Warehouse\n (3) Flatseal\n (4)outro (não esta pronto ainda)\n (5) sair\n");
-		scanf(" %d", &desi);
-		switch(desi){
-			case 1:
-				de1();
-				break;
-			case 2:
-				de2();
-				break;
-			case 3:
-				de3();
-				break;
-			case 4:
-				de4();
-				break;
-			case 5:
-				de5();
-				break;
-			
-		}
-	}
-	return 0;
+		printf("seja bem vindo ao atutil minha feramenta de auto instalacao para linux(Somente para ubuntu e linux mint, mas vou espandir para outras plataformas)\n");
+		 
+		// menu principal
 
+		printf("(1)Produtividade\n(2)jogos\n(3)dev(nao esta pronto ainda)\n(4)sair\n");
+		scanf("%d", &desi);
+		//caso for 1
+		if(desi == 1){
+			system("clear");
+			printf("qual programas voce deseja instalar?\n (1) ONLYOFFICE\n (2) Warehouse\n (3) Flatseal\n (4) sair\n");
+			scanf(" %d", &desi);
+			switch(desi){
+				case 1:
+					de1();
+					break;
+				case 2:
+					de2();
+					break;
+				case 3:
+					de3();
+					break;
+				case 4:
+					de4();
+					break;
+			}
+		}
+		// caso for 2
+		else if(desi == 2){
+			menu2();
+			scanf("%d", &d[0]);
+			switch(d[0]){
+				case 1:
+					insta1();
+					break;
+				case 2:
+					insta2();
+					break;
+				case 3:
+					insta3();
+					break;
+				case 4:
+					insta4();
+					break;
+			}
+		}
+		// caso for 3
+		else if(desi == 3){
+			// Menu dev vazio
+		}
+		// caso for 4
+		else if(desi == 4){
+			printf("fim do programa\n");
+			return 0;
+		}
+		
+		printf("\n");
+		printf("\ndeseja instalar mais alguma coisa? (s)sim (n)não: \n");
+		scanf(" %c", &resf);
+		
+		// onde o loop termina se o usuario quiser
+	}while(resf == 's' || resf == 'S');
+
+	printf("fim do programa\n");
+	
+	return 0;
 }
--- kew.h
+++ kew.h
@@ -2,23 +2,53 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int de1(){
-	system("flatpak install flathub org.onlyoffice.desktopeditors");
-
+//exibir menus 
+void menu2(){
+	system("clear");
+	printf("qual e o programa que voce deseja instalar?\n");
+	printf("(1)steam\n (2)Lutris\n (3)Heroic \n (4)cancelar\n");
 }
-int de2(){
+
+//install do menu 1 (sujeito a melhoras)
+void de1(){
+	system("flatpak install flathub org.onlyoffice.desktopeditors");
+}
+
+void de2(){
 	system("flatpak install flathub io.github.flattool.Warehouse");
 }
-int de3(){
-	system("flatpak install flathub com.github.tchx84.Flatseal");
 
+void de3(){
+	system("flatpak install flathub com.github.tchx84.Flatseal");
 }
+
 int de4(){
-	system("echo ainda não está pronto");
+	printf("fim do programa\n");
+	return 0;
 }
-int de5(){
-	printf("fim do programa");
+
+//install do menu 2
+void insta1(){
+	int a = 0;
+	printf("qual você quer instalar native(1) ou flatpak(2)\n");
+	scanf("%d", &a);
+	if(a == 1)
+	{
+		printf("ainda não disponivel");
+	}
+	else if(a == 2)
+	{
+		system("echo tudo certo");
+	}
 }
-int insta1(){
-	//novo tipo de istalação
+
+void insta2(){
+	system("echo tudo certo 2");
+}
+
+void insta3(){
+	system("echo tudo certo 3");
+}
+void insta4(){
+	printf("tudo certo");
 }
