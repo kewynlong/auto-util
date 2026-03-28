43aab3f9067e2dc7463d7c88689f75d10bc2be12
kewynlong <marcelojapao21@gmail.com>
2026-03-27 21:41:49 -0300

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>auto util</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <main class="topo">
+        seja bem vindo ao meu "projeto pessoal" para facilitar o uso de linux
+    </main>
+    <?php
+    
+    echo "<center>"; 
+    echo "ola mundo";
+    echo "<br>";
+    echo "teste de php";
+    echo "</center>"
+
+    ?>
+<body>
+</html>
