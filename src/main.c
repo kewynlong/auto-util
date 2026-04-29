4d8275a17db907412e6c735a5b6b64d412246358
kewynlong <marcelojapao21@gmail.com>
2026-04-29 19:16:43 -0300

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*
+*   dev: Kewynlong
+*   license: gpl
+*
+*   esté é um projeto pessoal, sem propsositos lucrativos, puramente para estudo e contribuição com a comunidade open-source
+*   está ferramenta templa ferramentas para automatizar/auto-instalar pacotes e configurar os proprios, seja lilvre para midificar os sripts
+*   e codigos desse programa para seu usso, mas eu gostaria que me desse creditos no seu codigo. 
+*
+*    se vc pagou por esté codigo vc foi enganado!
+*/
+
+
+//modulo principal, aqui sera a logica principal do codigo
+int main(){
+
+
+
+
+
+    return 0;
+}
